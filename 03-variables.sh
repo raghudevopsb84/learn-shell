d3acc780142bc2e6b8a3b1ddb75dcee7e02227e1
Raghu K <raghuk.vit@gmail.com>
2025-04-17 06:18:27 +0530

    RoboShop Project

---
+++ 03-variables.sh
@@ -0,0 +1,15 @@
+# If we assign a name to a set of data it is called as a variable.
+
+#Syntax: var=data
+x=10
+y=Hello
+
+#Access a variable
+#Syntax: $var or ${var}
+
+echo x - $x
+echo x - ${x}
+
+
+
+
