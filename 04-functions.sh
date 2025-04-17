9ed4ff4fe02e75bd3b39709f5ad4f46dd336ee34
Raghu K <raghuk.vit@gmail.com>
2025-04-17 06:48:13 +0530

    RoboShop Project

---
+++ 04-functions.sh
@@ -0,0 +1,16 @@
+# If we assign a name to a set of commands, then it is function.
+
+# syntax:
+# function_name() {
+#       commands
+#}
+
+# Access the function
+# function_name
+
+example() {
+  echo Hello from example function
+}
+
+example
+
