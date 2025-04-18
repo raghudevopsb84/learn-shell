a406e52dee158d6470cb72b28ccc475ed0b74db5
Raghu K <raghuk.vit@gmail.com>
2025-04-18 06:46:47 +0530

    RoboShop Project

---
+++ 06-loops.sh
@@ -0,0 +1,20 @@
+# IF we have to iterate some commands again and again then we go for loops
+
+# for     - If we have inputs
+# while   - If we have expressions
+
+# expressions discussed on if condition,. same expressions here for while loop
+
+# syntax:
+
+#while [ expression ]; do
+#  commands
+#done
+#
+
+x=10
+while [ $x -gt 0 ]; do
+  echo Loop in while command
+done
+
+
